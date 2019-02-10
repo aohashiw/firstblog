297ec133f7a0eb989fe319cebf89f976d16de904
LiuYiwei <131028133885@163.com>
2019-02-10 18:20:38 +0800

    send email

--- firstblog/settings.py
+++ firstblog/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ez%8=k!(c7(dt$kf8s&tqq#$$l3q!!u-m1d#&tkc9fa3)36+&&'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['*']
